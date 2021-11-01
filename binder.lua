a79dbec5047cd93bc8184882c7f38c5477f6baab
tedjblessave <71254284+tedjblessave@users.noreply.github.com>
2021-11-01 15:09:25 +0300

    Add files via upload

--- binder.lua
+++ binder.lua
@@ -33,7 +33,7 @@ local script_vers_text = "2.00"
 local update_url = "https://raw.githubusercontent.com/tedjblessave/binder/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
 
-local script_url = "https://github.com/thechampguess/scripts/blob/master/binder?raw=true" -- тут свою ссылку
+local script_url = "https://raw.githubusercontent.com/tedjblessave/binder/main/binder.lua" -- тут свою ссылку
 local script_path = thisScript().path
 
 --[[ local salosalo = false
