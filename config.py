b4eb69b80ff39b3df010284d332cd4671d61c341
tedjblessave <71254284+tedjblessave@users.noreply.github.com>
2023-03-22 00:50:25 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'TOKEN_VK': 'ТУТ ТОКЕН БОТА ВКОНТАКТЕ',
-    'TOKEN': 'MTAwMzY4NjY1NDU2Njk5MzkyMA.GmMcCm.SftpPaIcj97R8Tk2iuL46V6ldV6H5SRzwZU3Ts',
-    'GROUP_ID_VK_GHETTO': 'ТУТ ID БЕСЕДЫ ЛИДЕРОВ НЕЛЕГАЛОВ',
-    'GROUP_ID_VK_FAMILY': 'ТУТ ID БЕСЕДЫ ЛИДЕРОВ СЕМЕЙ'
-}
